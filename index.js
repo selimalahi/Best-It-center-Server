bb388e5213f54c77b01e4cdfea41c4be735cdbdd
selimalahi <alahi.selim@gmail.com>
2022-10-26 14:11:49 +0600

    news-categories.json update

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express =require('express')
 const app =express();
+const cors = require('cors');
 const port = process.env.PoRT || 5000;
 
+app.use(cors());
+
+const categories =require('./data/categories.json')
 
 app.get('/', (req, res) =>{
-    res.send('Course Api Running');
+    res.send('learning platform');
 });
 
+app.get('/course-categories', (req, res) =>{
+    res.send(categories)
+})
+
+app.listen(port, () => {
+    console.log('Course server Running on port', port);
+}) 
